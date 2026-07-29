d892a87729e7896612eeaac4447242286a1e58b9
nikolay.kozakov <nikolay.kozakov@mvideo.ru>
2026-07-29 18:52:41 +0300

    add notes

--- chan.go
+++ chan.go
@@ -1,21 +1,26 @@
 package eg
 
+// Chan - user-friendly chan without "<-" and "->"
 type Chan[T any] struct {
 	ch chan T
 }
 
+// NewChan - create a new Chan with data transfer type T
 func NewChan[T any]() *Chan[T] {
 	return &Chan[T]{}
 }
 
+// Original - get a go chan (not sure you really want it)
 func (c *Chan[T]) Original() chan T {
 	return c.ch
 }
 
+// Write - put a value in the Chan (blocking call until any eg.Thread or goroutine reads from it)
 func (c *Chan[T]) Write(v T) {
 	c.ch <- v
 }
 
+// Read - read a value from the Chan (blocking call until any eg.Thread or goroutine writes anything in it)
 func (c *Chan[T]) Read() T {
 	return <-c.ch
 }
--- slice.go
+++ slice.go
@@ -2,59 +2,69 @@ package eg
 
 import "sync"
 
+// Slice - user-friendly and concurrent-friendly slice []T without go-style crap like a = append(a, b)
 type Slice[T any] struct {
 	mtx sync.Mutex
 	sl  []T
 }
 
+// NewSlice - create new Slice of type T - elems may be empty (NOTE: returns a pointer)
 func NewSlice[T any](elems ...T) *Slice[T] {
 	return &Slice[T]{
 		sl: elems,
 	}
 }
 
+// NewSlice - create new Slice of type with given go-slice capacity (NOTE: returns a pointer)
 func NewSliceOfSize[T any](size int64) *Slice[T] {
 	return &Slice[T]{
 		sl: make([]T, 0, size),
 	}
 }
 
+// Original - returns go slice from the inside (just in case you need it)
 func (s *Slice[T]) Original() []T {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
 	return s.sl
 }
 
+// Size - size of go slice from the inside (not capacity)
 func (s *Slice[T]) Size() int64 {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
 	return int64(len(s.sl))
 }
 
+// IsEmpty - true if there are no values in it (capacity may not be 0)
 func (s *Slice[T]) IsEmpty() bool {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
 	return len(s.sl) == 0
 }
 
+// Vanish - completely vanishes the Slice - 0 len and 0 cap left
 func (s *Slice[T]) Vanish() {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
 	s.sl = []T{}
 }
 
+// ResetToDefault - replaces all the values with default of type T (NOTE: not recommended, better use Erase())
 func (s *Slice[T]) ResetToDefault() {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
 	s.sl = make([]T, s.Size())
 }
 
+// Erase - drops all the values (size will be 0) but keeps capacity the same as size was
 func (s *Slice[T]) Erase() {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
 	s.sl = make([]T, 0, s.Size())
 }
 
+// Get - panic-free "a = b[idx]" operation - returns value if idx is in bounds and default of type T otherwise
 func (s *Slice[T]) Get(idx int64) T {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
@@ -67,6 +77,7 @@ func (s *Slice[T]) Get(idx int64) T {
 	return s.sl[idx]
 }
 
+// Enlarge - adds some default values of T to the end, does nothing if newSize <= Size()
 func (s *Slice[T]) Enlarge(newSize int64) {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
@@ -79,6 +90,7 @@ func (s *Slice[T]) Enlarge(newSize int64) {
 	s.sl = append(s.sl, zeros...)
 }
 
+// Set - panic-free "b[idx] = a" operation - sets value if idx >= 0 and does nothing otherwise (NOTE: if idx >= Size(), it enlarges the Slice - see Enlarge())
 func (s *Slice[T]) Set(idx int64, value T) {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
@@ -94,6 +106,7 @@ func (s *Slice[T]) Set(idx int64, value T) {
 	s.sl[idx] = value
 }
 
+// Append - Python-styled "b = append(b, a)" operation - appends elems to the end of the inner go slice
 func (s *Slice[T]) Append(elems ...T) *Slice[T] {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
@@ -102,6 +115,7 @@ func (s *Slice[T]) Append(elems ...T) *Slice[T] {
 	return s
 }
 
+// Prepend - Python-styled "b = append(a, b)" operation - appends elems before the inner go slice
 func (s *Slice[T]) Prepend(elems ...T) *Slice[T] {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
@@ -110,6 +124,7 @@ func (s *Slice[T]) Prepend(elems ...T) *Slice[T] {
 	return s
 }
 
+// Extend - the same as Append but joins two objects of eg.Slice - returns the first of them and keeps another unchanged
 func (s *Slice[T]) Extend(another *Slice[T]) *Slice[T] {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
@@ -118,16 +133,13 @@ func (s *Slice[T]) Extend(another *Slice[T]) *Slice[T] {
 	return s
 }
 
+// Cut - panic-free slicing of the Slice - returns new Slice with the inner slice as example[start:end] (NOTE: start < end, start >= 0 and end < Size - otherwise it edits start and end to fit in bounds)
 func (s *Slice[T]) Cut(start, end int64) *Slice[T] {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
 
 	if start < 0 {
 		start = 0
-	}
-
-	if end < 0 {
-		end = 0
 	}
 
 	if start >= end {
@@ -141,4 +153,11 @@ func (s *Slice[T]) Cut(start, end int64) *Slice[T] {
 	sl := s.sl[start:end]
 
 	return NewSlice(sl...)
+}
+
+// Copy - safe copy of the Slice object (NOTE: lock is not copied, so the return Slice is completely independent)
+func (s *Slice[T]) Copy() *Slice[T] {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	return NewSlice(s.sl...)
 }
